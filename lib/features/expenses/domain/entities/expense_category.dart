14f7dcf77d06f216aa95f4ef857c281a0d52278d
salmanbashar1009 <salmanbashar101@gmail.com>
2026-08-29 17:37:00 +0600

    feat: add category model

---
+++ lib/features/expenses/domain/entities/expense_category.dart
@@ -0,0 +1,22 @@
+// lib/features/expenses/domain/entities/expense_category.dart
+enum ExpenseCategory {
+  food,
+  transport,
+  bills,
+  shopping,
+  entertainment,
+  health,
+  education,
+  other;
+
+  String get displayName {
+    return name[0].toUpperCase() + name.substring(1);
+  }
+
+  static ExpenseCategory fromString(String value) {
+    return ExpenseCategory.values.firstWhere(
+          (e) => e.name == value.toLowerCase(),
+      orElse: () => ExpenseCategory.other,
+    );
+  }
+}
